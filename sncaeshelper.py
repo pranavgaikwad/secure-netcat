6d37e4faa7c136ef9cc38c2df8c9664c7755241a
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2018-09-16 17:22:41 -0400

    fixed pylint errors

--- sncaeshelper.py
+++ sncaeshelper.py
@@ -3,7 +3,6 @@ Author   : Pranav Gaikwad
 Unity Id : 200203543
 '''
 
-import sys
 import json
 from base64 import b64encode, b64decode
 
@@ -11,57 +10,65 @@ from Crypto.Cipher import AES
 from Crypto.Protocol import KDF
 from Crypto.Random import get_random_bytes
 
+
 class IntegrityError(Exception):
+    """ custom error when integrity of message could not be guaranteed """
     pass
+
 
 class InvalidMessageError(Exception):
+    """ message could not be parsed / incomplete message received """
     pass
 
-class AesHelper:
-    ''' encryption / decryption helper '''
+
+class AesHelper(object):
+    """ encryption / decryption helper """
 
     # program does not allow keys less than 16 characters long
     LENGTH_IDEAL_KEY = 16
 
     # these keys are expected in a typical encrypted msg
-    MSG_KEYS = [ 'n', 'c', 't', 's' ]
+    MSG_KEYS = ['n', 'c', 't', 's']
 
     @staticmethod
     def encrypt(plaintext, key):
-        ''' encrypts data using AES in GCM mode '''
-        ''' and returns msg with tag, nonce, ciphertext & salt in it '''
+        """
+        encrypts data using AES in GCM mode
+        and returns msg with tag, nonce, ciphertext & salt in it
+        """
         # generate a random salt or pepper or whatever
         salt = get_random_bytes(AesHelper.LENGTH_IDEAL_KEY)
-        # derive key 
+        # derive key
         key = AesHelper._derive_key(key, salt)
         # encrypt
         cipher = AES.new(key, AES.MODE_GCM)
         ciphertext, tag = cipher.encrypt_and_digest(plaintext)
-        values = [ b64encode(value).decode('utf-8') for value in cipher.nonce, ciphertext, tag, salt ]
-        
+        values = [b64encode(value).decode('utf-8') for value in cipher.nonce, ciphertext, tag, salt]
         return json.dumps(dict(zip(AesHelper.MSG_KEYS, values)))
 
     @staticmethod
     def decrypt_and_verify(ciphertext, key):
-        ''' decrypts data encoded using AES '''
+        """ decrypts data encoded using AES """
         try:
             b64 = json.loads(ciphertext)
-            msg = { key: b64decode(b64[key]) for key in AesHelper.MSG_KEYS }
-        except Exception as e:
-            raise InvalidMessageError('Invalid message : %s\n'%str(e))
+            msg = {key: b64decode(b64[key]) for key in AesHelper.MSG_KEYS}
+        except Exception as error:
+            raise InvalidMessageError('Invalid message : %s\n' % str(error))
 
         key = AesHelper._derive_key(key, msg[AesHelper.MSG_KEYS[3]])
-        
+
         cipher = AES.new(key, AES.MODE_GCM, nonce=msg[AesHelper.MSG_KEYS[0]])
-        
-        try: 
-            plaintext = cipher.decrypt_and_verify(msg[AesHelper.MSG_KEYS[1]], msg[AesHelper.MSG_KEYS[2]])
-        except Exception as e:
-            raise IntegrityError('Integrity of message is compromised : %s'%str(e))
+
+        try:
+            plaintext = cipher.decrypt_and_verify(
+                msg[AesHelper.MSG_KEYS[1]], msg[AesHelper.MSG_KEYS[2]])
+        except Exception as error:
+            raise IntegrityError(
+                'Integrity of message is compromised : %s' % str(error))
 
         return plaintext
 
     @staticmethod
     def _derive_key(password, salt):
-        ''' derives PBKDF2 key from given key & password '''
+        """ derives PBKDF2 key from given key & password """
         return KDF.PBKDF2(password, salt, dkLen=32)
--- sncargparser.py
+++ sncargparser.py
@@ -8,56 +8,67 @@ import argparse
 
 from sncaeshelper import AesHelper
 
-def parse_snc_args():
-    # custom usage message
-    USAGE_MSG = 'snc.py [-h] [-l] --key KEY server port'
-    
-    # parses CLI arguments
-    argparser = argparse.ArgumentParser(usage=USAGE_MSG)
-    # positional arguments
-    # Refer https://docs.python.org/2/library/argparse.html#nargs for nargs argument
-    argparser.add_argument("connection", nargs='*', help="Connection string in format [server] [port]")
-    # other args
-    argparser.add_argument("-l", help="listen mode / start as client", action="store_true", default=False)
-    argparser.add_argument("--key", help="encryption key", required=True)
-    
-    args = argparser.parse_args()
-    
-    # 'connection' argument is a list of all arguments remained after parsing --key & -l
-    # it could be server & port values; or just a port value when '-l' is set
-    connection_string_list = args.connection
-    
-    # checks if '-l' argument is set. 
-    # if yes, program will run in 'LISTEN' mode
-    LISTEN = args.l 
-    
-    HOST = ''
-    PORT = ''
-    KEY = args.key
 
-    if len(KEY) < AesHelper.LENGTH_IDEAL_KEY:
-        sys.stderr.write('Key should be at least %s characters long\n'%str(AesHelper.LENGTH_IDEAL_KEY))
+def parse_snc_args():
+    """
+    parses command line arguments
+    """
+    # custom usage message
+    usage_msg = 'snc.py [-h] [-l] --key KEY server port'
+
+    # parses CLI arguments
+    argparser = argparse.ArgumentParser(usage=usage_msg)
+    # positional arguments
+    # Refer https://docs.python.org/2/library/argparse.html#nargs
+    argparser.add_argument("connection", nargs='*',
+                           help="Connection string in format [server] [port]")
+    # other args
+    argparser.add_argument(
+        "-l", help="listen mode / start as client",
+        action="store_true", default=False)
+    argparser.add_argument("--key", help="encryption key", required=True)
+
+    args = argparser.parse_args()
+
+    # 'connection' argument is a list of all
+    # arguments remained after parsing --key & -l
+    # it could be server & port values; or just a
+    # port value when '-l' is set
+    connection_string_list = args.connection
+
+    # checks if '-l' argument is set.
+    # if yes, program will run in 'listen' mode
+    listen = args.l
+
+    host = ''
+    port = ''
+    key = args.key
+
+    if len(key) < AesHelper.LENGTH_IDEAL_KEY:
+        sys.stderr.write('Key should be at least %s characters long\n' %
+                         str(AesHelper.LENGTH_IDEAL_KEY))
         sys.exit(1)
-    
+
     # if 'listen' mode is set, connection_string_list only has PORT
     # in it. If not, connection_string_list needs to be parsed
-    # as SERVER & PORT. this is due to limitation of 'argparser'
+    # as SERVER & port. this is due to limitation of 'argparser'
     # module which does not support conditional positional variables
-    if LISTEN:
+    if listen:
         try:
-            PORT = int(connection_string_list[0])
+            port = int(connection_string_list[0])
         except IndexError:
             sys.stderr.write('Please specify a port\n')
             sys.exit(1)
     else:
         try:
-            HOST = connection_string_list[0]
-            PORT = int(connection_string_list[1])
+            host = connection_string_list[0]
+            port = int(connection_string_list[1])
         except IndexError:
-            sys.stderr.write('Please specify connection string in [server] [port] format\n')
+            sys.stderr.write(
+                'Please specify connection string in [server] [port] format\n')
             sys.exit(1)
         except ValueError:
             sys.stderr.write('Incorrect host or port\n')
             sys.exit(1)
 
-    return HOST, PORT, LISTEN, KEY
+    return host, port, listen, key
